dbd9751c913021f55959aeb307758926d1a3da36
Maxime Forget-Séguin <maxime.forget-seguin@radio-canada.ca>
2016-05-24 13:20:35 -0400

    update to koco2

--- src/images-dialog-ui.js
+++ src/images-dialog-ui.js
@@ -1,76 +1,77 @@
 // Copyright (c) CBC/Radio-Canada. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-define(["text!./images-dialog.html", "knockout", 'configs'], function(template, ko, configs) {
-    var ViewModel = function(params, componentInfo) {
-        var self = this;
-        var undef;
+import template from "text!./images-dialog.html";
+import ko from "knockout";
+import configs from 'configs';
+var ViewModel = function(params, componentInfo) {
+    var self = this;
+    var undef;
 
-        self.title = params.title;
+    self.title = params.title;
 
-        var imagesBasePath = (configs.baseUrl || '') + 'bower_components/koco-demo-image-picker/src/images/';
+    var imagesBasePath = (configs.baseUrl || '') + 'bower_components/koco-demo-image-picker/src/images/';
 
-        //todo: should we throw instead?
-        if (configs.imagePicker && configs.imagePicker.imagesBasePath) {
-            imagesBasePath = configs.imagePicker.imagesBasePath;
-        }
+    //todo: should we throw instead?
+    if (configs.imagePicker && configs.imagePicker.imagesBasePath) {
+        imagesBasePath = configs.imagePicker.imagesBasePath;
+    }
 
-        self.titleFocused = ko.observable(true);
+    self.titleFocused = ko.observable(true);
 
-        self.images = ko.observableArray([{
-            name: '1',
-            value: imagesBasePath + '1.jpg'
-        }, {
-            name: '2',
-            value: imagesBasePath + '2.jpg'
-        }, {
-            name: '3',
-            value: imagesBasePath + '3.jpg'
-        }]);
+    self.images = ko.observableArray([{
+        name: '1',
+        value: imagesBasePath + '1.jpg'
+    }, {
+        name: '2',
+        value: imagesBasePath + '2.jpg'
+    }, {
+        name: '3',
+        value: imagesBasePath + '3.jpg'
+    }]);
 
-        self.content = ko.validatedObservable({
-            image: ko.observable(undef).extend({
-                required: {
-                    message: "Image is required. "
-                },
-                successValidatingMessage: {
-                    validatingMessage: "Validating...",
-                    validMessage: "Value is valid!"
-                }
-            }),
-            title: ko.observable('').extend({
-                required: {
-                    message: "Title is required. "
-                },
-                exampleAsync: {},
-                successValidatingMessage: {
-                    validatingMessage: "Validating...",
-                    validMessage: "Value is valid!"
-                }
-            })
-        }).extend({
-            bootstrapValidation: {}
-        });
-
-        self.close = function() {
-            params.close();
-        };
-
-        self.save = function() {
-            self.content.isValidAsync().then(function(isValid) {
-                if (isValid) {
-                    params.close(self.content().image());
-                }
-            });
-        };
-    };
-
-    return {
-        viewModel: {
-            createViewModel: function(params, componentInfo) {
-                return new ViewModel(params, componentInfo);
+    self.content = ko.validatedObservable({
+        image: ko.observable(undef).extend({
+            required: {
+                message: "Image is required. "
+            },
+            successValidatingMessage: {
+                validatingMessage: "Validating...",
+                validMessage: "Value is valid!"
             }
-        },
-        template: template
+        }),
+        title: ko.observable('').extend({
+            required: {
+                message: "Title is required. "
+            },
+            exampleAsync: {},
+            successValidatingMessage: {
+                validatingMessage: "Validating...",
+                validMessage: "Value is valid!"
+            }
+        })
+    }).extend({
+        bootstrapValidation: {}
+    });
+
+    self.close = function() {
+        params.close();
     };
-});
+
+    self.save = function() {
+        self.content.isValidAsync().then(function(isValid) {
+            if (isValid) {
+                params.close(self.content().image());
+            }
+        });
+    };
+};
+
+export default {
+    viewModel: {
+        createViewModel: function(params, componentInfo) {
+            return new ViewModel(params, componentInfo);
+        }
+    },
+    template: template
+};
--- src/koco-demo-image-picker-ui.js
+++ src/koco-demo-image-picker-ui.js
@@ -1,53 +1,56 @@
 // Copyright (c) CBC/Radio-Canada. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-define(['text!./koco-demo-image-picker.html', 'jquery', 'knockout-utilities', 'dialoger', 'knockout', 'configs'],
-    function(template, $, knockoutUtilities, dialoger, ko, configs) {
-        "use strict";
+import template from 'text!./koco-demo-image-picker.html';
+import $ from 'jquery';
+import knockoutUtilities from 'knockout-utilities';
+import dialoger from 'dialoger';
+import ko from 'knockout';
+import configs from 'configs';
 
-        var ViewModel = function(params, componentInfo) {
-            var self = this;
 
-            var imagesBasePath = (configs.baseUrl || '/') + 'bower_components/koco-demo-image-picker/src/images/';
+var ViewModel = function(params, componentInfo) {
+    var self = this;
 
-            //todo: should we throw instead?
-            if (configs.imagePicker && configs.imagePicker.imagesBasePath) {
-                imagesBasePath = configs.imagePicker.imagesBasePath;
-            }
+    var imagesBasePath = (configs.baseUrl || '/') + 'bower_components/koco-demo-image-picker/src/images/';
 
-            self.focused = ko.observable(false);
-            self.imageUrl = params.image;
+    //todo: should we throw instead?
+    if (configs.imagePicker && configs.imagePicker.imagesBasePath) {
+        imagesBasePath = configs.imagePicker.imagesBasePath;
+    }
 
-            if (!self.imageUrl()) {
-                self.imageUrl(imagesBasePath + 'choisir2.png');
-            }
-        };
+    self.focused = ko.observable(false);
+    self.imageUrl = params.image;
 
-        ViewModel.prototype.selectImage = function(model, jQueryEvent) {
-            var self = this;
+    if (!self.imageUrl()) {
+        self.imageUrl(imagesBasePath + 'choisir2.png');
+    }
+};
 
-            if (!ko.components.isRegistered('images-dialog')) {
-                dialoger.registerDialog('images', {
-                    title: 'Select an image',
-                    basePath: 'bower_components/koco-demo-image-picker/src'
-                });
-            }
+ViewModel.prototype.selectImage = function(model, jQueryEvent) {
+    var self = this;
 
-            dialoger.show('images').then(function(imageUrl) {
-                if (imageUrl) {
-                    self.imageUrl(imageUrl);
-                }
+    if (!ko.components.isRegistered('images-dialog')) {
+        dialoger.registerDialog('images', {
+            title: 'Select an image',
+            basePath: 'bower_components/koco-demo-image-picker/src'
+        });
+    }
 
-                self.focused(true);
-            });
-        };
+    dialoger.show('images').then(function(imageUrl) {
+        if (imageUrl) {
+            self.imageUrl(imageUrl);
+        }
 
-        return {
-            viewModel: {
-                createViewModel: function(params, componentInfo) {
-                    return new ViewModel(params, componentInfo);
-                }
-            },
-            template: template
-        };
+        self.focused(true);
     });
+};
+
+export default {
+    viewModel: {
+        createViewModel: function(params, componentInfo) {
+            return new ViewModel(params, componentInfo);
+        }
+    },
+    template: template
+};
